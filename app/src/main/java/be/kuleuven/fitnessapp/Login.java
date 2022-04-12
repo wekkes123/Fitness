8c8b46eeb4888e35d8d2668896496acb13830abb
LAPTOP-PKRHRB5J\vanlo <vanloo.wout@gmail.com>
2022-04-12 11:53:53 +0200

    update

--- app/src/main/java/be/kuleuven/fitnessapp/Login.java
+++ app/src/main/java/be/kuleuven/fitnessapp/Login.java
@@ -3,11 +3,15 @@ package be.kuleuven.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    private TextView LoginLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LoginLogo = (TextView) findViewById(R.id.LoginLogo);
+        LoginLogo.bringToFront();
     }
 }
